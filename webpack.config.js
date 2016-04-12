cd8e19c226de8079c1dcb7eab647451884755b5f
Yury Kozyrev <yury.kozyrev@home24.de>
2016-04-12 17:10:01 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path');
+var webpack = require('webpack');
+var WebpackMd5Hash    = require('webpack-md5-hash');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-module.exports = {
+test = {
   entry: [
     path.normalize('es6-shim/es6-shim.min'),
     'reflect-metadata',
@@ -10,7 +12,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve('www/build/js'),
-    filename: 'app.bundle.js',
+    filename: 'unitrans.[chunkhash].js',
     pathinfo: false // show module paths in the bundle, handy for debugging
   },
   module: {
@@ -46,8 +48,62 @@ module.exports = {
   resolve: {
     root: ['app'],
     alias: {
-      'angular2': path.resolve('node_modules/angular2')
+      'angular2': path.resolve('node_modules/angular2'),
     },
     extensions: ["", ".js", ".ts"]
   }
+};
+
+// https://github.com/ghillert/angular2-webpack-starter-bootstrap
+module.exports = {
+  entry: [
+    path.normalize('es6-shim/es6-shim.min'),
+    'reflect-metadata',
+    path.normalize('zone.js/dist/zone'),
+    './app/boot.ts',
+  ],
+  output: {
+    path: path.resolve('build/webpack'),
+    filename: 'unitrans.js',
+  },
+  //devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'awesome-typescript'},
+    ],
+    noParse: [
+      /es6-shim/,
+      /reflect-metadata/,
+      /zone\.js(\/|\\)dist(\/|\\)zone/
+    ]
+  },
+
+  plugins: [
+    new WebpackMd5Hash(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'polyfills',
+    //   filename: 'polyfills.[chunkhash].bundle.js',
+    //   chunks: Infinity
+    // }),
+    new webpack.optimize.UglifyJsPlugin({
+
+      beautify: false,//prod
+      mangle: false,
+      compress : { screw_ie8 : true},//prod
+      comments: false//prod
+
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index_prod.tpl.html',
+      filename: 'index.html',
+      inject:false,
+      hash: true,
+      cache:false
+    })
+  ]
 };
